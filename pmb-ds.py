e4e09f4f6d13a5c677ecae3c21ac09668f0d999c
sriram chaudhury <sriram.chaudhury@oracle.com>
2022-09-05 14:42:18 +0530

    updated prog to read with simple tuple

--- pmb-ds.py
+++ pmb-ds.py
@@ -2,77 +2,57 @@ import sys
 import re
 
 
-def get_key(counter, line):
-    wd, catg, wnc = line.split()
-    key = str(counter) + '_' + catg
+def get_key(counter, first_col):
+    # get uniq key for the dictionary
+
+    if len(first_col.split('.')) > 2:
+        wd, catg, wnc = first_col.split('.')
+        key = str(counter) + '_' + catg
+    else:
+        key = str(counter) + '_' + first_col
     return key
 
+
 def chk_valid(line):
-    if not re.match(r'^%%%.*', line):
+    # check if the line is valid to read and store in the Data structure
+
+    if not re.match(r'^%%%.*', line) and len(line.strip())>0:
         return True
 
+
 def read_sbn_line(line):
-    #For each line in sbn get word, concept, wnc, role
 
-    first_col, second_col = line.split('\t')[:2]
-    wd, catg, wnc = first_col.split('.')
-    role_tuple = tuple((l[i], l[i + 1]) for i in range(0, len(l), 2))
+    # For each line in sbn get word, concept, wnc, role
 
-    return
+    info_line =  line.split('%')[0]
+    elements = info_line.split()
+    wn_concept = elements[0]
+    second_col = elements[1:]
+
+    if len(second_col) > 1:
+        role_tuple = tuple((second_col[i], second_col[i + 1]) for i in range(0, len(second_col), 2))
+    elif len(second_col) == 1:
+        role_tuple = (second_col[0])
+    else:
+        role_tuple = ()
+    return (wn_concept, role_tuple)
+
 
 def read_sbn(pmb_cont):
     #Read all rows in the sbn format in pmb annotation and store
     # it in a dicitionary of nested tuples format
 
     pmb_ds = {}
+    index = 0
     for line in pmb_cont:
         if chk_valid(line):
-            key = get_key(counter, line)
+            first_col = line.split()[0]
+            key = get_key(index, first_col)
             pmb_ds[key] = read_sbn_line(line)
+            index += 1
 
     return pmb_ds
 
-
-def read_pmb(sbn_all_rows):
-    #Read all rows in the sbn format in pmb annotation and store
-    # it in a dicitionary of nested tuples format
-
-    pmb_ds = {}
-    index = 0
-    for row in sbn_all_rows:
-        if not re.match(r'^%%%.*', row) and len(
-                row.strip().split()) > 0:  # to not consider empty lines and comments in sbn
-
-            try:
-                # get all sbn information in a nested tuple format
-
-                m = re.match(r'([^\s]*)\s+([^%]*)\s*(.*)', row)
-                lex = m.group(1)
-                role = m.group(2).strip()
-                if len(lex.split('.')) > 2:
-                    lex_word, lex_catg, lex_concept = lex.split('.')
-                    if re.match(r'.*Name.*', role):
-                        m = re.match(r'.*Name\s+"(.*)".*', role)
-                        role_tuple = ('Name', m.group(1))
-                    else:
-                        role_tuple = ()
-                        l = role.split()
-                        if len(l) > 0:
-                            role_tuple = tuple((l[i], l[i + 1]) for i in range(0, len(l), 2))
-                else:
-                    role_tuple = role
-                    lex_catg = lex
-
-                key = str(index) + "_" + lex_catg
-                pmb_ds[key] = (lex, role_tuple)
-                index += 1
-
-            except:
-
-                print("ERROR in format of SBN: %s" % row)
-                exit()
-
-    return pmb_ds
 
 def find_all_verb_arg(key, pmb_ds):
     index = int(re.match(r'(\d+)_', key)[1])
@@ -94,6 +74,7 @@ def find_all_verb_arg(key, pmb_ds):
             break
     return tuple(list_arg)
 
+
 def find_verb_arg(verb, rel_name, pmb_ds):
     for key in pmb_ds:
         if re.match(r'.*v_*', key):
@@ -111,8 +92,10 @@ def find_verb_arg(verb, rel_name, pmb_ds):
 def get_gnp(line, pmb_ds):
     print('')
 
+
 def get_vib(line, pmb_ds):
     print('')
+
 
 def add_gnp_vib(pmb_ds):
     #add gender, number, person information for each word in English for
@@ -127,14 +110,15 @@ def add_gnp_vib(pmb_ds):
 
     return pmb_ds
 
+
 def get_hnd_wd(line, pmb_ds):
 
-    #open the file in Hindi wordnet dir and read the hnd meaning.
-    #Apply additional rules given by the linguist
+    # open the file in Hindi wordnet dir and read the hnd meaning.
+    # Apply additional rules given by the linguist
 
     wd = ''
-
     return wd
+
 
 def add_hindi_wd(pmb_ds):
     # for each concept word get the equivalent Hindi word from the Hindi 'Wordnet'
@@ -146,18 +130,18 @@ def add_hindi_wd(pmb_ds):
     return pmb_ds
 
 
-
 def gen_hindi(pmb_ds):
     print('')
+
 
 if __name__ == '__main__':
 
     # read the pmb file and store in pmb data structure
     f = open(sys.argv[1], 'r')
     pmb_cont = f.readlines()
-    pmb_ds = read_pmb(pmb_cont)
+    pmb_ds = read_sbn(pmb_cont)
 
-    #Get Hindi word from Hindi wordnet (Verbnet Dir)
+    # Get Hindi word from Hindi wordnet (Verbnet Dir)
     add_hindi_wd(pmb_ds)
     for key in pmb_ds:
         print(key,pmb_ds[key])
